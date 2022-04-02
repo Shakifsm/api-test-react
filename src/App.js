d4903a265f17deaeedb40874cb8fbb9c03fb71a3
shakifsm <shakif20@gmail.com>
2022-04-02 17:26:52 +0600

    wsad

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   useEffect(()=>{
     const getData = async () => {
       const response = await fetch(
-        `https://api-test-react.vercel.app/website/findCategoryAndProducts?isActive=true&storeId=1-2021`
+        `http://18.170.51.237:3000/website/findCategoryAndProducts?isActive=true&storeId=1-2021`
       );
       const data = await response.json();
       setSidebarList(data);
