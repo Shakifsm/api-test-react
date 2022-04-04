da8265ef49086ae7ce12e661f97febc2da6d8535
shakifsm <shakif20@gmail.com>
2022-04-04 15:59:53 +0600

    api test

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [ sidebarList, setSidebarList ] = useState([]);
-  console.log(sidebarList);
+  const [ restaurant, setRestaurant ] = useState([]);
+  console.log(restaurant);
   
   useEffect(()=>{
     const getData = async () => {
       const response = await fetch(
-        `http://18.170.51.237:3000/website/findCategoryAndProducts?isActive=true&storeId=1-2021`
+        `https://devoretapi.co.uk/api/v1/website/getRestaurantContent/5f0c174d7c3bf80552f07947`
       );
       const data = await response.json();
-      setSidebarList(data);
+      setRestaurant(data);
     };
     getData();
   }, [])
@@ -19,8 +19,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       {
+       {/* {
          sidebarList?.categories?.map(item => <li key={item._id}>{item.categoryName}</li>)
+       } */}
+       <h1>{restaurant?.restaurant_data?.restaurant_name}</h1>
+       {
+         restaurant?.restaurant_data?.schedule.map( item =>
+         <div key={item._id}>
+           <h6>{item.day_name}</h6>
+           
+             {
+               item?.default?.map( time => <h6 key={time._id}>{time.opening_time} <br /> {time.closing_time} </h6> )
+             }
+           
+         </div> 
+        )
        }
       </header>
     </div>
